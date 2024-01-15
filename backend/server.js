1fccdd17a48f6a3f207c8b07387cd73838aae820
@oumar1994 <oumarfilston@gmail.com>
2024-01-14 18:07:47 -0800

    change directory in the main

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ app.use(cors())
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/messages",messageRoutes)
-app.use(express.static(path.join(__dirname,'./frontend/dist')));
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, './frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 }) 
 const PORT = process.env.PORT || 5000;
 
